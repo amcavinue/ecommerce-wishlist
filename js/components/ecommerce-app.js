64d3f64a37091c0e3781cf165310de6a69b03abf
Aaron <aaronmcavinue@gmail.com>
2017-01-15 22:02:29 +0000

    Add build files before branching.

--- js/components/ecommerce-app.js
+++ js/components/ecommerce-app.js
@@ -23,7 +23,7 @@ const EcommerceApp = React.createClass({
         
             <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul className="nav navbar-nav navbar-right">
-                <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
+                <li className="active"><a href="#">Link</a></li>
                 <li><a href="#">Link</a></li>
                 <li><a href="#">Link</a></li>
               </ul>
