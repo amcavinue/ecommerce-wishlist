ac1029fd48063994a29bc98d3695503e0db214b4
Aaron <aaronmcavinue@gmail.com>
2017-01-17 00:46:48 +0000

    Add tests for login and new user components.

---
+++ test/test-login.js
@@ -0,0 +1,32 @@
+const React = require('react');
+const TestUtils = require('react-addons-test-utils');
+const chai = require('chai');
+const should = chai.should();
+const expect = chai.expect;
+const Provider = require('react-redux').Provider;
+
+const store = require('../js/store');
+const actions = require('../js/actions/index');
+const Login = require('../js/components/login');
+
+describe('Login Component', () => {
+  it('Renders the component',  () => {
+    let renderer = TestUtils.createRenderer();
+    let callback = () => {
+      return true;
+    };
+    
+    renderer.render(
+      <Login showNewUser={callback} />
+    );
+    let result = renderer.getRenderOutput();
+    
+    // Test the Login component.
+    TestUtils.isElementOfType(result, Login);
+    TestUtils.isCompositeComponent(result);
+    
+    // Test nested elements.
+    expect(result.props.children.length).to.equal(2);
+    expect(result.props.children[0].type).to.equal('h1');
+  });
+});
