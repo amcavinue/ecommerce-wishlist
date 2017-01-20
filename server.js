c58dd480d8d4607753579faf6e82b791f2aadc4d
Aaron <aaronmcavinue@gmail.com>
2017-01-20 00:58:46 +0000

    Add encrypted environment variables to Travis.

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const amazon = require('amazon-product-api');
+var process = require('process');
 
-var config = require('./config');
+/*process.env = Object.assign(process.env, require('./amazon-config'));
+const config = require('./config');*/
 
-var app = express();
+const app = express();
 app.use(express.static('build')); // Serve the build folder.
 app.use('/modules', express.static(__dirname + '/node_modules/')); // Serve the node_modules folder.
 app.use(bodyParser.json()); // Used for getting parameters in post requests.
 
-var runServer = function(callback) {
+/**
+ * Helper Functions
+ */
+// Client for accessing the Amazon Product Advertising API.
+const client = amazon.createClient({
+  awsTag: process.env.AWS_TAG,
+  awsId: process.env.AWS_ID,
+  awsSecret: process.env.AWS_SECRET
+});
+
+/**
+ * Routes
+ */
+
+/**
+ * Run the server
+ */
+const runServer = function(callback) {
+  console.log(process.env, 33);
     mongoose.connect(config.DATABASE_URL, function(err) {
         if (err && callback) {
             return callback(err);
@@ -32,5 +53,8 @@ if (require.main === module) {
     });
 };
 
+/**
+ * Exports
+ */
 exports.app = app;
 exports.runServer = runServer;
