9830d848cf052f498e90ee731cbe3ce5b5e8d4a0
Aaron <aaronmcavinue@gmail.com>
2017-01-26 22:39:54 +0000

    Mockup product list.

--- js/components/product-search.js
+++ js/components/product-search.js
@@ -15,6 +15,18 @@ const ProductSearch = React.createClass({
       data1: null
     };
   },
+  componentDidMount() {
+    $('#list').click((e) => {
+      e.preventDefault();
+      $('#products .item').addClass('list-group-item');
+    });
+    
+    $('#grid').click((e) => {
+      e.preventDefault();
+      $('#products .item').removeClass('list-group-item');
+      $('#products .item').addClass('grid-group-item');
+    });
+  },
   submit(e) {
     e.preventDefault();
   },
@@ -29,7 +41,146 @@ const ProductSearch = React.createClass({
             <input className="form-control" type="text" id="search" name="search" required/>
           </div>
           <input className="btn btn-primary" type="submit" value="Submit" name="submit" />
-      </form>
+        </form>
+        
+        <div className="container list-grid-view">
+            <div className="well well-sm">
+                <strong>Display </strong>
+                <div className="btn-group">
+                    <a href="" id="list" className="btn btn-default btn-sm"><span className="glyphicon glyphicon-th-list">
+                    </span>List</a> <a href="" id="grid" className="btn btn-default btn-sm"><span
+                        className="glyphicon glyphicon-th"></span>Grid</a>
+                </div>
+            </div>
+            <div id="products" className="row list-group">
+                <div className="item col-xs-12 col-md-6 col-lg-4">
+                    <div className="thumbnail">
+                        <img className="group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" />
+                        <div className="caption">
+                            <h4 className="group inner list-group-item-heading">
+                                Product title</h4>
+                            <p className="group inner list-group-item-text">
+                                Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                                sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                            <div className="row">
+                                <div className="col-xs-12 col-md-6">
+                                    <p className="lead">
+                                        $21.000</p>
+                                </div>
+                                <div className="col-xs-12 col-md-6">
+                                    <a className="btn btn-success" href="">Add to cart</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="item col-xs-12 col-md-6 col-lg-4">
+                    <div className="thumbnail">
+                        <img className="group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" />
+                        <div className="caption">
+                            <h4 className="group inner list-group-item-heading">
+                                Product title</h4>
+                            <p className="group inner list-group-item-text">
+                                Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                                sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                            <div className="row">
+                                <div className="col-xs-12 col-md-6">
+                                    <p className="lead">
+                                        $21.000</p>
+                                </div>
+                                <div className="col-xs-12 col-md-6">
+                                    <a className="btn btn-success" href="">Add to cart</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="item col-xs-12 col-md-6 col-lg-4">
+                    <div className="thumbnail">
+                        <img className="group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" />
+                        <div className="caption">
+                            <h4 className="group inner list-group-item-heading">
+                                Product title</h4>
+                            <p className="group inner list-group-item-text">
+                                Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                                sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                            <div className="row">
+                                <div className="col-xs-12 col-md-6">
+                                    <p className="lead">
+                                        $21.000</p>
+                                </div>
+                                <div className="col-xs-12 col-md-6">
+                                    <a className="btn btn-success" href="">Add to cart</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="item col-xs-12 col-md-6 col-lg-4">
+                    <div className="thumbnail">
+                        <img className="group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" />
+                        <div className="caption">
+                            <h4 className="group inner list-group-item-heading">
+                                Product title</h4>
+                            <p className="group inner list-group-item-text">
+                                Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                                sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                            <div className="row">
+                                <div className="col-xs-12 col-md-6">
+                                    <p className="lead">
+                                        $21.000</p>
+                                </div>
+                                <div className="col-xs-12 col-md-6">
+                                    <a className="btn btn-success" href="">Add to cart</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="item col-xs-12 col-md-6 col-lg-4">
+                    <div className="thumbnail">
+                        <img className="group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" />
+                        <div className="caption">
+                            <h4 className="group inner list-group-item-heading">
+                                Product title</h4>
+                            <p className="group inner list-group-item-text">
+                                Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                                sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                            <div className="row">
+                                <div className="col-xs-12 col-md-6">
+                                    <p className="lead">
+                                        $21.000</p>
+                                </div>
+                                <div className="col-xs-12 col-md-6">
+                                    <a className="btn btn-success" href="">Add to cart</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="item col-xs-12 col-md-6 col-lg-4">
+                    <div className="thumbnail">
+                        <img className="group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" />
+                        <div className="caption">
+                            <h4 className="group inner list-group-item-heading">
+                                Product title</h4>
+                            <p className="group inner list-group-item-text">
+                                Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                                sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                            <div className="row">
+                                <div className="col-xs-12 col-md-6">
+                                    <p className="lead">
+                                        $21.000</p>
+                                </div>
+                                <div className="col-xs-12 col-md-6">
+                                    <a className="btn btn-success" href="">Add to cart</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
       </div>
     );
   }
